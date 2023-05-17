475e03073d4cfea659cd9b3dc1bbd22ed48b4b5b
yanyan <shinya_suzuki@voyagegroup.com>
2023-05-17 01:56:53 +0000

    docker-compose

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("pong")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
 	})
 
 	if err := http.ListenAndServe(":80", nil); err != nil {
